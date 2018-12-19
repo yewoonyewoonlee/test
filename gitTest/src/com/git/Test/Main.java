4919525cb06147e67054dc480ee5623057d5f53b
user1 <user1@user1-PC>
2018-12-19 13:18:13 +0900

    Main출력문 수정

--- gitTest/src/com/git/Test/Main.java
+++ gitTest/src/com/git/Test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("git 처음이야!");
+		System.out.println("여러분 힘내세요 할수있다!");
 
 	}
 
